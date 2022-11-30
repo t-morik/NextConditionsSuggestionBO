321ec18586654730aaa07fbd2cf0fe256fbd2844
Yujin Taguchi <taguchi.yujin@gmail.com>
2022-11-30 14:37:37 +0900

    Confirmed that initial condtions can be set

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ domain = [{'name': 'time of stimulation', 'type': 'discrete', 'domain': (2,3,4,5
             {'name': 'frequency', 'type': 'continuous', 'domain': (0.5, 10)},
             {'name': 'voltage', 'type': 'continuous', 'domain': (0, 18)}]
 
+my_initial_design = np.array([
+    [1 , 3.5, 5.6],
+    [1 , 3.5, 5.9],
+    [1 , 3.5, 10],
+    [1 , 3.5, 15]
+])
+
 # --- Solve your problem
-myBopt = BayesianOptimization(f=f, domain=domain)
+myBopt = BayesianOptimization(f=f, domain=domain, X=my_initial_design)
 myBopt.run_optimization(max_iter=15)
 
 print(f'{myBopt.X}')

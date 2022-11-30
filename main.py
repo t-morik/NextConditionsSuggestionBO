941eb82a1bcf47397b894cc65e014415396375ab
Yujin Taguchi <taguchi.yujin@gmail.com>
2022-12-01 03:21:37 +0900

    Add comment

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ domain = [{'name': 'time of stimulation', 'type': 'discrete', 'domain': (2,3,4,5
             {'name': 'frequency', 'type': 'continuous', 'domain': (0.5, 10)},
             {'name': 'voltage', 'type': 'continuous', 'domain': (0, 18)}]
 
+# condtions.txtのファイルから実験条件と結果の組み合わせを取ってくる。
 data = np.loadtxt('./conditions.txt', delimiter= ",", skiprows=1)
-conditions = data[:, 0:3]
+conditions = data[:, 0:3] # 末列以外の部分で実験条件の設定を行なっている。
 
-# --- Define your problem
+# 実験条件の組み合わせを受け取ったら結果を返す関数(すでに実験済みの条件のみ)
 def f(x, data = data):
     for i in range(0,3):
         if np.array_equal(x, data[i][0:3]):
             return data[i][3]
 
-# --- Solve your problem
+# クラスのインスタンス化と次の実験条件の提案
 myBopt = BayesianOptimization(f=f, domain=domain, X=conditions, batch_size=4, evaluator_type = 'local_penalization')
 result = myBopt.suggest_next_locations()
 

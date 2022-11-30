d18928b9187a6164b237ada4c663b7c0050226e1
Yujin Taguchi <taguchi.yujin@gmail.com>
2022-11-30 17:41:18 +0900

    Rebuild an envirionment and suggest next experimental condition

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 from GPyOpt.methods import BayesianOptimization
 import numpy as np
 
-# --- Define your problem
-def f(x): return np.sum(x) # TODO
-
+# 条件を探索する領域
 domain = [{'name': 'time of stimulation', 'type': 'discrete', 'domain': (2,3,4,5,6,7,8)},
             {'name': 'frequency', 'type': 'continuous', 'domain': (0.5, 10)},
             {'name': 'voltage', 'type': 'continuous', 'domain': (0, 18)}]
@@ -12,8 +10,14 @@ domain = [{'name': 'time of stimulation', 'type': 'discrete', 'domain': (2,3,4,5
 data = np.loadtxt('./conditions.txt', delimiter= ",", skiprows=1)
 conditions = data[:, 0:3]
 
-# --- Solve your problem
-myBopt = BayesianOptimization(f=f, domain=domain, X=conditions)
-myBopt.run_optimization(max_iter=15)
+# --- Define your problem
+def f(x, data = data):
+    for i in range(0,3):
+        if np.array_equal(x, data[i][0:3]):
+            return data[i][3]
 
-print(f'{myBopt.X}')
+# --- Solve your problem
+myBopt = BayesianOptimization(f=f, domain=domain, X=conditions, batch_size=4, evaluator_type = 'local_penalization')
+result = myBopt.suggest_next_locations()
+
+print(f'{ result = }')

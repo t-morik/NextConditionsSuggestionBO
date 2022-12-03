97ad56fa641357adefebcd859f5cd347d9a2a2b5
Yujin Taguchi <taguchi.yujin@gmail.com>
2022-12-04 00:46:57 +0900

    Set f None

--- main.py
+++ main.py
@@ -11,19 +11,14 @@ domain = [{'name': 'time of stimulation', 'type': 'discrete', 'domain': (0,1,2,3
 # condtions.txtのファイルから実験条件と結果の組み合わせを取ってくる。
 data = np.loadtxt('./conditions.csv', delimiter= ",", skiprows=1)
 conditions = data[:, 0:3] # 末列以外の部分で実験条件の設定を行なっている。
-
-# 実験条件の組み合わせを受け取ったら結果を返す関数(すでに実験済みの条件のみ)
-def f(x, data = data):
-    for i in range(0,3):
-        if np.array_equal(x, np.array([data[i][0:3]])):
-            return data[i][3]
-    return -1 # 実験していないものに関してはあり得ない数値を返す
+results = -1 * data[:, 3].reshape(len(data),1)
 
 # クラスのインスタンス化と次の実験条件の提案
 seed(123)
-myBopt = BayesianOptimization(f=f, 
+myBopt = BayesianOptimization(f=None,
                                 domain=domain, 
                                 X=conditions, 
+                                Y=results,
                                 batch_size=4, 
                                 evaluator_type = 'local_penalization',
                                 maximize=True)
